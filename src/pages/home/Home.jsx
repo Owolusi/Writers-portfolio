2d2691e41ef60e8b00c429e012efbb28a3d3d514
Owolusi Lucky <109273986+Owolusi@users.noreply.github.com>
2023-10-08 21:02:35 +0100

    first commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import Profile from "../../assets/home.jpg";
+import { Link } from 'react-router-dom';
+import { FaArrowRight } from 'react-icons/fa';
+import "./home.css"
+
+
+const Home = () => {
+  return (
+    <section className='home section grid'>
+      <img src={Profile} alt='' className="home__img"/>
+        <div className="home__content">
+          <div className="home__data">
+            <h1 className='home__title'>
+              <span>
+              I'm Owolusi Lucky <br/>
+              </span>
+              web Designer
+            </h1>
+            <p className="home__description">
+            a passionate front-end developer weaving digital dreams into reality.<br/>            
+             With an artistic flair for design and a code-crafter mindset, <br/>
+             I bring ideas to life on the web. Let's embark on a journey where pixels meet purpose,<br/>
+              creating immersive and user-centric experiences. <br/>
+            Welcome to my digital realm!
+            </p>
+            <Link to='/about' className='button'>
+              More About me<span className='button__icon'>
+                <FaArrowRight
+                />
+              </span>
+            </Link>
+          </div>
+
+        </div>
+    </section>
+  )
+}
+
+export default Home
